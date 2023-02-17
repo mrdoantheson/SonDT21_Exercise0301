c450d04bed34c3dc6056965fe511c590712a26ef
SonDT21 <mr.doantheson@gmail.com>
2023-02-17 13:41:39 +0700

    First commit

---
+++ src/exercise/Calculator.java
@@ -0,0 +1,24 @@
+package exercise;
+
+/**
+ * The class to calculate the sum, subtraction, multiplication, and division of two integer numbers.
+ *
+ * @author SonDT21
+ */
+public class Calculator {
+    public int sum(int number1, int number2) {
+        return (number1 + number2);
+    }
+
+    public int sub(int number1, int number2) {
+        return (number1 - number2);
+    }
+
+    public int mul(int number1, int number2) {
+        return (number1 * number2);
+    }
+
+    public int div(int number1, int number2) {
+        return (number1 / number2);
+    }
+}
